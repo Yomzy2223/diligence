e776d4042e85617fcb64ce02b13b899a9e16dfd4
yomzy2223 <oluwolesayo10@gmail.com>
2023-07-23 15:08:31 +0100

    Fixed Storybook module not found issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import CorporateRequest from "@/components/features/corporate/corporateRequest/CorporateRequest";
+import CorporateRequest from "@/components/features/corporateRequest/CorporateRequest";
 import Image from "next/image";
 
 export default function Home() {
--- src/components/features/corporateRequest/CorporateRequest.stories.tsx
+++ src/components/features/corporateRequest/CorporateRequest.stories.tsx
@@ -18,8 +18,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  args: {
-    primary: true,
-    label: "Button",
-  },
+  args: {},
 };
